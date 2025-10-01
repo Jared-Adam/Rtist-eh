80c9ba1fa564c0fc72b343e9c28997303122aef6
Jared Adam <jared.adam@student.montana.edu>
2025-10-01 09:56:08 -0600

    partay start

---
+++ Scripts/trial_run.r
@@ -0,0 +1,52 @@
+# Art? 
+
+library(tidyverse)
+
+# blank canvas ####
+
+axis_min <- 0
+axis_max <- 10
+
+base_data <- tibble(x = axis_min:axis_max,
+                    y =x)
+base_data %>% 
+  ggplot(aes(x,y))
+
+# add points ####
+
+base_data %>% 
+  ggplot(aes(x,y))+ 
+  geom_point()
+
+grid_points <- expand.grid(base_data)
+
+grid_points %>% 
+  ggplot(aes(x,y))+
+  geom_point()
+
+# spacing ####
+
+polk <- grid_points %>% 
+  mutate( x = case_when(y %% 2 == 0 & x %% 2 > 0 ~ NA_integer_,
+                        TRUE ~ x),
+          y = case_when( x %% 2 == 0 & y %% 2 > 0 ~ NA_integer_,
+                         TRUE ~ y)) %>% 
+  filter(if_all(everything(), ~!is.na(.)))
+
+polk %>% 
+  ggplot(aes(x,y))+
+  geom_point()
+
+# aes ####
+
+dot_size <- 10
+dot_color <- "#ffffff"
+back_col <- "#598c4c"
+
+polk %>% 
+  ggplot(aes(x,y))+
+  theme_void()+ # remove axes and labels
+  geom_point(size = 10, 
+             color = dot_color)+
+  theme(plot.background = element_rect(fill = back_col))
+
